946666ee1a9cea46a9ef4edf44d64aa9770da76b
Thomas Leconte <thomasl.dw20@gmail.com>
2023-12-30 15:01:58 +0100

    Graph BAR : info bulle style corrigé

--- src/components/recharts/SimpleBarChart/SimpleBarChart.jsx
+++ src/components/recharts/SimpleBarChart/SimpleBarChart.jsx
@@ -19,13 +19,22 @@ const CustomTooltip = ({active, payload}) => {
 
         return (
             <div style={{
-                backgroundColor: 'rgba(255, 255, 255, 0.8)',
-                padding: '10px',
-                borderRadius: '5px',
-                border: '1px solid #ccc'
+                // backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                backgroundColor: '#E60000',
+                color: '#fff',
+                // padding: '20px 0px 20px 0px',
+                marginTop: '-120px',
+                marginLeft: '40px',
+                // borderRadius: '5px',
+                border: '1px solid #ccc',
+                // textAlign: 'center',
+                display: 'flex',
+                flexDirection: 'column',
+                // justifyContent: 'center',
+                alignItems: 'center'
             }}>
-                {kilogram && <p style={{margin: 0}}>{`${kilogram.value}kg`}</p>}
-                {calories && <p style={{margin: 0}}>{`${calories.value}kCal`}</p>}
+                {kilogram && <p style={{marginTop: 10,marginBottom:10, padding: 15}}>{`${kilogram.value}kg`}</p>}
+                {calories && <p style={{marginTop: 10,marginBottom:10, padding: 15}}>{`${calories.value}kCal`}</p>}
             </div>
         );
     }
