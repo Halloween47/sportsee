7b9f8aae991c5e25fd40064a79a5b407b53efffa
Thomas Leconte <thomasl.dw20@gmail.com>
2023-12-30 23:57:29 +0100

    Graph SCORE : Correction design

--- src/components/recharts/PieChart/PieChartPerso.jsx
+++ src/components/recharts/PieChart/PieChartPerso.jsx
@@ -1,16 +1,27 @@
+import React from "react";
 import {PieChart, Pie, Label, ResponsiveContainer} from 'recharts';
 
 function PieChartPerso(props) {
     let data = props.dataUser;
     let todayScore;
     if (data[0].todayScore) {
-        todayScore = data[0].todayScore * 100 + "% de votre objectif";
+        todayScore = data[0].todayScore * 100;
     } else {
-        todayScore = data[0].score * 100 + "% de votre objectif";
-
+        // todayScore = data[0].score * 100 + "% de votre objectif";
+        todayScore = data[0].score * 100;
     }
     let resultatAngleTodayScore = parseInt(todayScore) * 360 / 100;
     const dataKeyToUse = data[0].todayScore ? "todayScore" : "score";
+
+    const CustomLabel = ({ viewBox, value1, value2 }) => {
+        const { cx, cy } = viewBox;
+        return (
+            <g>
+                <text id="score" x={cx} y={cy - 10} textAnchor="middle">{value1}%</text>
+                <text x={cx} y={cy + 15} textAnchor="middle">{value2}</text>
+            </g>
+        );
+    };
 
     return (
         <ResponsiveContainer width="100%" height="100%"
@@ -31,7 +42,8 @@ function PieChartPerso(props) {
                     cornerRadius={10}
                     // isAnimationActive={true}
                 >
-                    <Label value={todayScore} position="center"/>
+                    {/* <Label value={todayScore} position="center"/> */}
+                    <Label content={<CustomLabel value1={todayScore.toFixed(0)} value2="de votre objectif" />} position="center" />           
                 </Pie>
             </PieChart>
         </ResponsiveContainer>
